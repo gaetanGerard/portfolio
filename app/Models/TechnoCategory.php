25fc489794dac00e95b96e380a298978dddd45a1
gaetan <gaetan.gerard27@gmail.com>
2024-05-31 15:26:03 +0200

    add model and migration for technologies and categories

---
+++ app/Models/TechnoCategory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TechnoCategory extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    public function technologies()
+    {
+        return $this->hasMany(Technologies::class, 'category_id');
+    }
+}
---
+++ app/Models/Technologies.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Technologies extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'category_id',
+        'icon_path',
+        'technology_url',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(TechnoCategory::class, 'category_id');
+    }
+}
