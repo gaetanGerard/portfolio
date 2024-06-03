cf2ff2a19d56d06378394e5cd4c3173ecadbe3eb
gaetan <gaetan.gerard27@gmail.com>
2024-06-03 18:22:02 +0200

    ajout dashboard stat

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,98 +1,67 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head, usePage } from '@inertiajs/react';
-import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 
 export default function Dashboard({ auth }) {
-    const {projects, technoCategory, technologies} = usePage().props;
-
-    const handleDeleteProject = async (id) => {
-        try {
-            const response = await axios.delete(`/admin/dashboard/projects/delete/${id}`);
-            window.location.reload();
-            console.log(response.data.message);
-        } catch (error) {
-            console.error('Une erreur est survenue lors de la suppression du projet :', error);
-        }
-    };
-
-    const handleDeleteCategory = async (id) => {
-        try {
-            const response = await axios.delete(`/admin/dashboard/categories/delete/${id}`);
-            window.location.reload();
-            console.log(response.data.message);
-        } catch (error) {
-            console.error('Une erreur est survenue lors de la suppression de la catégorie :', error);
-        }
-    };
-
-    const handleDeleteTechnology = async (id) => {
-        try {
-            const response = await axios.delete(`/admin/dashboard/technologies/delete/${id}`);
-            window.location.reload();
-            console.log(response.data.message);
-        } catch (error) {
-            console.error('Une erreur est survenue lors de la suppression de la technologie :', error);
-        }
-    };
+    const {projects, technoCategory, technologies, experiences, educations} = usePage().props;
 
     return (
         <AuthenticatedLayout
             user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Projets & Technologies</h2>}
+            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
         >
             <Head title="Dashboard" />
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <ul className="p-6 text-gray-900">
-                            {projects.length > 0 ? projects.map((project, index) => (
-                                <li key={index}>
-                                    <h3>Titre : {project.title}</h3>
-                                    <img src={`${project.main_img}`} alt={`${project.title}`} />
-                                    <p>Description : {project.short_description}</p>
-                                    {project.demo_link != null ? (<p>Demo Link: {project.demo_link}</p>) : null}
-                                    {project.github_repo != null ? (<p>Github: {project.github_repo}</p>) : null}
-                                    <div className="button-group">
-                                        <Button variant="contained" className="mt-2" color="warning" href={`/admin/dashboard/projects/edit?id=${project.id}`}>Modifier</Button>
-                                        <Button variant="contained" className="mt-2" color="error" onClick={() => handleDeleteProject(project.id)}>Supprimer</Button>
-                                    </div>
-                                </li>
-                            ))  : <p>Aucun projet n'a été trouvé.</p>}
-                        </ul>
+                    <Typography variant="h3" component="h1" className="text-3xl font-bold text-gray-800">Tableau de bord</Typography>
+                    <div className="mt-6 mb-4 border-b border-gray-200">
+                        <Typography variant="h4" component="h2" className="text-2xl font-bold text-gray-800">Statistiques</Typography>
                     </div>
-                    <Button variant="contained" className="my-2" href='/admin/dashboard/projects/add'>Ajouter un projet</Button>
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <ul className="p-6 text-gray-900">
-                            {technologies.length > 0 ? technologies.map((technology, index) => (
-                                    <li key={index}>
-                                        <h3>Nom : {technology.name}</h3>
-                                        <img src={`${technology.icon_path}`} alt={`${technology.name}`} style={{ width: '100px', height: '100px', objectFit: 'cover' }}  />
-                                        <div className="button-group">
-                                            <Button variant="contained" className="mt-2" color="warning" href={`/admin/dashboard/technologies/edit?id=${technology.id}`}>Modifier</Button>
-                                            <Button variant="contained" className="mt-2" color="error" onClick={() => handleDeleteTechnology(technology.id)}>Supprimer</Button>
-                                        </div>
-                                    </li>
-                                ))  : <p>Aucune technologie n'a été trouvé.</p>}
-                        </ul>
+                    <div>
+                        <Card>
+                            <CardContent>
+                                <Typography variant="h5" component="div">Projets</Typography>
+                                {projects.length > 0 ? (<Typography variant="body2" component="p">
+                                    {projects.length}
+                                </Typography>) : <Typography variant="body2" component="p">Aucun projet</Typography>}
+                            </CardContent>
+                        </Card>
+                        <Card>
+                            <CardContent>
+                                <Typography variant="h5" component="div">Technologies</Typography>
+                                {technologies.length > 0 ? (<Typography variant="body2" component="p">
+                                    {technologies.length}
+                                </Typography>) : <Typography variant="body2" component="p">Aucune technologie</Typography>}
+                            </CardContent>
+                        </Card>
+                        <Card>
+                            <CardContent>
+                                <Typography variant="h5" component="div">Catégories de technologies</Typography>
+                                {technoCategory.length > 0 ? (<Typography variant="body2" component="p">
+                                    {technoCategory.length}
+                                </Typography>) : <Typography variant="body2" component="p">Aucune catégorie de technologie</Typography>}
+                            </CardContent>
+                        </Card>
+                        <Card>
+                            <CardContent>
+                                <Typography variant="h5" component="div">Expériences</Typography>
+                                {experiences.length > 0 ? (<Typography variant="body2" component="p">
+                                    {experiences.length}
+                                </Typography>) : <Typography variant="body2" component="p">Aucune expérience</Typography>}
+                            </CardContent>
+                        </Card>
+                        <Card>
+                            <CardContent>
+                                <Typography variant="h5" component="div">Formations</Typography>
+                                {educations.length > 0 ? (<Typography variant="body2" component="p">
+                                    {educations.length}
+                                </Typography>) : <Typography variant="body2" component="p">Aucune formation</Typography>}
+                            </CardContent>
+                        </Card>
                     </div>
-                    <Button variant="contained" className="my-2" href='/admin/dashboard/technologies/add'>Ajouter une technologie</Button>
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <ul className="p-6 text-gray-900">
-                            {technoCategory.length > 0 ? technoCategory.map((category, index) => (
-                                <li key={index}>
-                                    <h3>Nom : {category.name}</h3>
-                                    <p>Description : {category.description}</p>
-                                    <div className="button-group">
-                                        <Button variant="contained" className="mt-2" color="warning" href={`/admin/dashboard/categories/edit?id=${category.id}`}>Modifier</Button>
-                                        <Button variant="contained" className="mt-2" color="error" onClick={() => handleDeleteCategory(category.id)}>Supprimer</Button>
-                                    </div>
-                                </li>
-                            ))  : <p>Aucune catégorie n'a été trouvé.</p>}
-                        </ul>
-                    </div>
-                    <Button variant="contained" className="my-2" href='/admin/dashboard/categories/add'>Ajouter une catégorie</Button>
-
                 </div>
             </div>
         </AuthenticatedLayout>
