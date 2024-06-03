2b3a4fa8ba48119833c4d720feb0efcbda589a73
gaetan <gaetan.gerard27@gmail.com>
2024-06-03 09:04:24 +0200

    add experiences migration & model

---
+++ database/migrations/2024_06_03_065907_create_experiences_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('experiences', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('company_name');
+            $table->string('company_location');
+            $table->string('job_title');
+            $table->date('start_date');
+            $table->date('end_date')->nullable();
+            $table->boolean('is_current')->default(false);
+            $table->text('description')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('experiences');
+    }
+};
