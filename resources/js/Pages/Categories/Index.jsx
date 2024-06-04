9f643458ed17cb7fe2df7b9944aa47b7cc29cec7
gaetan <gaetan.gerard27@gmail.com>
2024-06-04 23:41:12 +0200

    complete design for categories

--- resources/js/Pages/Categories/Index.jsx
+++ resources/js/Pages/Categories/Index.jsx
@@ -1,19 +1,123 @@
+import {useState, useEffect} from 'react';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head, usePage } from '@inertiajs/react';
 import Button from '@mui/material/Button';
+import InfoIcon from '@mui/icons-material/Info';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import IconButton from '@mui/material/IconButton';
+import Snackbar from '@mui/material/Snackbar';
+import Alert from '@mui/material/Alert';
+import { DataGrid } from '@mui/x-data-grid';
+import { frFR } from '@mui/x-data-grid/locales';
+import CustomNoRowsOverlay from '@/Components/CustomNoRowOverlay';
+import Tooltip from '@mui/material/Tooltip';
 
 const Index = ({ auth }) => {
     const {categories} = usePage().props;
+    const [open, setOpen] = useState(false);
+    const [message, setMessage] = useState('');
+    const [severity, setSeverity] = useState('');
+
+    useEffect(() => {
+        const storedMessage = localStorage.getItem('snackbarMessage');
+        const storedOpen = localStorage.getItem('snackbarState') === 'true';
+        const storedSeverity = localStorage.getItem('snackbarSeverity');
+
+        if (storedMessage && storedOpen && storedSeverity !== null) {
+            setMessage(storedMessage);
+            setOpen(storedOpen);
+            setSeverity(storedSeverity);
+        }
+
+        if(open) {
+            const timeoutId = setTimeout(() => {
+                localStorage.removeItem('snackbarMessage');
+                localStorage.removeItem('snackbarState');
+                localStorage.removeItem('snackbarSeverity');
+
+                setMessage('');
+                setSeverity('');
+                setOpen(false);
+            }, 5000);
+
+            return () => clearTimeout(timeoutId);
+        }
+
+    }, [open]);
 
     const handleDeleteCategory = async (id) => {
         try {
             const response = await axios.delete(`/admin/dashboard/categories/delete/${id}`);
+            const message = response.data.message;
+            const open = true;
+            const severity = 'success';
+            localStorage.setItem('snackbarMessage', message);
+            localStorage.setItem('snackbarState', open);
+            localStorage.setItem('snackbarSeverity', severity);
             window.location.reload();
-            console.log(response.data.message);
         } catch (error) {
+            const message = 'Une erreur est survenue lors de la suppression du projet.';
+            const open = true;
+            const severity = 'error';
+            localStorage.setItem('snackbarMessage', message);
+            localStorage.setItem('snackbarState', open);
+            localStorage.setItem('snackbarSeverity', severity);
             console.error('Une erreur est survenue lors de la suppression de la catégorie :', error);
         }
     };
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+          return;
+        }
+
+        localStorage.removeItem('snackbarMessage');
+        localStorage.removeItem('snackbarState');
+        localStorage.removeItem('snackbarSeverity');
+        setOpen(false);
+    };
+
+    const columns = [
+        { field: 'id', headerName: 'ID', width: 90 },
+        { field: 'name', headerName: 'Nom', width: 150, flex: 1},
+        { field: 'description', headerName: 'Description', width: 150, flex: 1 },
+        {
+            field: 'actions',
+            headerName: 'Actions',
+            width: 150,
+            flex: 1,
+            renderCell: (params) => (
+                <div className="flex items-center justify-center">
+                    <Tooltip title="Détail" placement="top">
+                        <IconButton href={`/admin/dashboard/categories/category/${params.row.id}`}>
+                            <InfoIcon />
+                        </IconButton>
+                    </Tooltip>
+                    <Tooltip title="Modifier" placement="top">
+                        <IconButton href={`/admin/dashboard/categories/edit?id=${params.row.id}`}>
+                            <EditIcon />
+                        </IconButton>
+                    </Tooltip>
+                    <Tooltip title="Supprimer" placement="top">
+                        <IconButton onClick={() => handleDeleteCategory(params.row.id)}>
+                            <DeleteIcon />
+                        </IconButton>
+                    </Tooltip>
+                </div>
+            )
+        }
+    ];
+
+    const rows = categories.map((category) => {
+        return {
+            id: category.id,
+            name: category.name,
+            description: category.description,
+        };
+    });
+
+
   return (
     <AuthenticatedLayout
         user={auth.user}
@@ -23,20 +127,21 @@ const Index = ({ auth }) => {
 
         <div className="py-12">
             <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <ul className="p-6 text-gray-900">
-                        {categories.length > 0 ? categories.map((category, index) => (
-                            <li key={index}>
-                                <h3>Nom : {category.name}</h3>
-                                <p>Description : {category.description}</p>
-                                <div className="button-group">
-                                    <Button variant="contained" className="mt-2" href={`/admin/dashboard/categories/category/${category.id}`}>Détail</Button>
-                                    <Button variant="contained" className="mt-2" color="warning" href={`/admin/dashboard/categories/edit?id=${category.id}`}>Modifier</Button>
-                                    <Button variant="contained" className="mt-2" color="error" onClick={() => handleDeleteCategory(category.id)}>Supprimer</Button>
-                                </div>
-                            </li>
-                        ))  : <p>Aucune catégorie n'a été trouvé.</p>}
-                    </ul>
+                <Snackbar open={open} autoHideDuration={5000} onClose={handleClose}>
+                    <Alert onClose={handleClose} severity={severity} sx={{ width: '100%' }}>
+                        {message}
+                    </Alert>
+                </Snackbar>
+                <div style={{ height: 371, width: '100%' }}>
+                    <DataGrid
+                        rows={rows}
+                        columns={columns}
+                        pageSize={5}
+                        rowsPerPageOptions={[5, 10, 25, 50, 100]}
+                        pageSizeOptions={[5, 10, 25, 50, 100]}
+                        localeText={frFR.components.MuiDataGrid.defaultProps.localeText}
+                        slots={{ noRowsOverlay: () => (<CustomNoRowsOverlay message='Aucune technologie trouvé.' />) }}
+                    />
                 </div>
                 <Button variant="contained" className="my-2" href='/admin/dashboard/categories/add'>Ajouter une catégorie</Button>
             </div>
