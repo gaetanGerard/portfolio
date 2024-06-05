fe92f4af9030f8898b5b07236a94b6c15be96f56
gaetan <gaetan.gerard27@gmail.com>
2024-06-05 11:19:09 +0200

    complete design of experience detail page

--- resources/js/Pages/Experiences/Index.jsx
+++ resources/js/Pages/Experiences/Index.jsx
@@ -119,8 +119,6 @@ export default function Index({ auth }) {
         };
     });
 
-    console.log(experiences);
-
     return (
         <AuthenticatedLayout
             user={auth.user}
--- resources/js/Pages/Experiences/Show.jsx
+++ resources/js/Pages/Experiences/Show.jsx
@@ -1,18 +1,75 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head, usePage, router } from '@inertiajs/react';
 import Button from '@mui/material/Button';
 import dayjs from 'dayjs';
+import Snackbar from '@mui/material/Snackbar';
+import Alert from '@mui/material/Alert';
+import Tooltip from '@mui/material/Tooltip';
+import IconButton from '@mui/material/IconButton';
+import LanguageIcon from '@mui/icons-material/Language';
 
 const Show = ({ auth  }) => {
     const { experience } = usePage().props;
+    const [open, setOpen] = useState(false);
+    const [message, setMessage] = useState('');
+    const [severity, setSeverity] = useState('');
+
+    useEffect(() => {
+        const storedMessage = localStorage.getItem('snackbarMessage');
+        const storedOpen = localStorage.getItem('snackbarState') === 'true';
+        const storedSeverity = localStorage.getItem('snackbarSeverity');
+
+        if (storedMessage && storedOpen && storedSeverity !== null) {
+            setMessage(storedMessage);
+            setOpen(storedOpen);
+            setSeverity(storedSeverity);
+        }
+
+        if(open) {
+            const timeoutId = setTimeout(() => {
+                localStorage.removeItem('snackbarMessage');
+                localStorage.removeItem('snackbarState');
+                localStorage.removeItem('snackbarSeverity');
+
+                setMessage('');
+                setSeverity('');
+                setOpen(false);
+            }, 5000);
+
+            return () => clearTimeout(timeoutId);
+        }
+
+    }, [open]);
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+          return;
+        }
+
+        localStorage.removeItem('snackbarMessage');
+        localStorage.removeItem('snackbarState');
+        localStorage.removeItem('snackbarSeverity');
+        setOpen(false);
+    };
 
     const handleDeleteExperience = async (id) => {
         try {
             const response = await axios.delete(`/admin/dashboard/experiences/delete/${id}`);
+            const message = response.data.message;
+            const open = true;
+            const severity = 'success';
+            localStorage.setItem('snackbarMessage', message);
+            localStorage.setItem('snackbarState', open);
+            localStorage.setItem('snackbarSeverity', severity);
             window.location.href = '/admin/dashboard/experiences';
-            console.log(response.data.message);
         } catch (error) {
+            const message = 'Une erreur est survenue lors de la suppression de l\'expérience.';
+            const open = true;
+            const severity = 'error';
+            localStorage.setItem('snackbarMessage', message);
+            localStorage.setItem('snackbarState', open);
+            localStorage.setItem('snackbarSeverity', severity);
             console.error('Une erreur est survenue lors de la suppression de l\'expérience :', error);
         }
     };
@@ -20,18 +77,27 @@ const Show = ({ auth  }) => {
   return (
     <AuthenticatedLayout
     user={auth.user}
-    header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Expériences Professionel</h2>}
+    header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Expériences Professionel : {experience.company_name}</h2>}
 >
     <Head title="Expériences Professionel" />
-    <div>
-        <p>Nom de l'entreprise : <span>{experience.company_name}</span></p>
-        <p>Titre du job : <span>{experience.job_title}</span></p>
-        <p>Lieu : <span>{experience.company_location}</span></p>
-        <p>Début : <span>{dayjs(experience.start_date).format('DD/MM/YYYY')}</span></p>
-        {experience.is_current ? (<p>En cours : Oui</p>) : (<p>Fin : <span>{dayjs(experience.end_date).format('DD/MM/YYYY')}</span></p>)}
-        <p>Description : <span>{experience.description}</span></p>
-        <Button variant="contained" className="mt-2" color="warning" href={`/admin/dashboard/experiences/edit?id=${experience.id}`}>Modifier</Button>
-        <Button variant="contained" className="mt-2" color="error" onClick={() => handleDeleteExperience(experience.id)}>Supprimer</Button>
+    <div className="m-3">
+        <Snackbar open={open} autoHideDuration={5000} onClose={handleClose}>
+            <Alert onClose={handleClose} severity={severity} sx={{ width: '100%' }}>
+                {message}
+            </Alert>
+        </Snackbar>
+        <div className="grid lg:grid-cols-3 gap-3 md:grid-cols-1 bg-white overflow-hidden shadow-sm sm:rounded-lg p-3">
+            <p className="font-bold">Nom de l'entreprise : <span className="font-normal">{experience.company_name}</span></p>
+            <p className="font-bold">Titre du job : <span className="font-normal">{experience.job_title}</span></p>
+            <p className="font-bold">Lieu : <span className="font-normal">{experience.company_location}</span></p>
+            <p className="font-bold">Début : <span className="font-normal">{dayjs(experience.start_date).format('DD/MM/YYYY')}</span></p>
+            {experience.is_current ? (<p className="font-bold">En cours : Oui</p>) : (<p className="font-bold">Fin : <span className="font-normal">{dayjs(experience.end_date).format('DD/MM/YYYY')}</span></p>)}
+            <p className="font-bold col-span-3">Description : <span className="font-normal">{experience.description}</span></p>
+        </div>
+        <div className="grid grid-flow-col justify-start gap-2">
+            <Button variant="contained" className="mt-2" color="warning" href={`/admin/dashboard/experiences/edit?id=${experience.id}`}>Modifier</Button>
+            <Button variant="contained" className="mt-2" color="error" onClick={() => handleDeleteExperience(experience.id)}>Supprimer</Button>
+        </div>
     </div>
 </AuthenticatedLayout>
   )
