1ff186cf0e33c33dd29a1801641ba133d7bbfbb5
gaetan <gaetan.gerard27@gmail.com>
2024-06-20 15:06:46 +0200

    working on projects component

--- resources/js/Pages/Portfolio.jsx
+++ resources/js/Pages/Portfolio.jsx
@@ -4,6 +4,7 @@ import Nav from '@/Components/Nav';
 import HeaderSection from './Sections/HeaderSection';
 import About from './Sections/About';
 import Skills from './Sections/Skills';
+import Projects from './Sections/Projects';
 
 export default function Portfolio() {
     const { localeData, user, categories, technologies, categoryTechnologies } = usePage().props;
@@ -27,6 +28,7 @@ export default function Portfolio() {
                 <HeaderSection data={data} userName={user.name} />
                 <About description={language === "fr" ? user.fr_description : user.en_description} picture={user.user_img} />
                 <Skills categoryTechnologies={categoryTechnologies} language={language} data={data} />
+                <Projects data={data} />
             </>
         </>
     );
--- resources/js/Pages/Sections/Projects.jsx
+++ resources/js/Pages/Sections/Projects.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-const Projects = () => {
+const Projects = ({data}) => {
+    const section = data.projects;
   return (
-    <div id="projects">Projects</div>
+    <div id="projects" className="bg-custom-dark p-5 w-full h-full">
+        <div className="grid grid-flow-row gap-3 text-white justify-center justify-items-center">
+            <h1>{section.title}</h1>
+            <p className="w-50 text-center">{section.description}</p>
+        </div>
+    </div>
   )
 }
 
