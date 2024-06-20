c199107fec2aefe05560bc408dd793728dd2f2f0
gaetan <gaetan.gerard27@gmail.com>
2024-06-20 09:05:34 +0200

    add download button to headerSection

--- resources/js/Pages/Sections/HeaderSection.jsx
+++ resources/js/Pages/Sections/HeaderSection.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from 'react';
 import Typed from "typed.js";
 import ParticleBackground from '@/Components/ParticleBackground';
+import { Link } from '@inertiajs/react'
 
 const HeaderSection = ({data, userName}) => {
   const el = useRef(null);
@@ -27,6 +28,7 @@ const HeaderSection = ({data, userName}) => {
     <div className="relative flex items-center justify-center h-screen">
             <div className="absolute top-50 left-50 text-white z-50 text-center">
               <div className="text-3xl" ref={el} />
+              <Link href="TO DEFINE" className="uppercase border-2 p-3 mt-3 hover:border-highlight hover:text-highlight transition duration-500 ease-in-out" as="button" preserveScroll >{section.btnText}</Link>
             </div>
         <ParticleBackground />
     </div>
